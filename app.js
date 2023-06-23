e6636a1b71ada43459e5dd3ed7f1b055d14d5b7b
sahil <sahiltyagi1204@outlook.com>
2023-06-23 19:31:38 +0530

    final steps code

--- app.js
+++ app.js
@@ -4,15 +4,43 @@ const score = document.querySelector('#score')
 const time = document.querySelector('#time')
 
 let result = 0
+let hitPosition ;
+let currentTime = 60
 
 function randomSquares (){
     squares.forEach(square =>{
         square.classList.remove('mole')
-
     })
+
     let randomPosition = squares[Math.floor(Math.random()*9)]
-    console.log(randomPosition);
     randomPosition.classList.add('mole')
+    hitPosition = randomPosition.id
+}
+    squares.forEach(square =>{
+        square.addEventListener('mousedown' , ()=>{
+            if(square.id == hitPosition){
+                result++
+                score.innerHTML = result
+                hitPosition = null
+            }
+        })
+    })
+
+
+function moveMole(){
+    timerId = null;
+    timerId= setInterval(randomSquares , 1000)
+}
+// moveMole()
+
+function countDown(){
+    currentTime --
+    time.innerHTML= currentTime
+    if(currentTime == 0){
+        clearInterval(countDownTimerId )
+        clearInterval(timerId)
+        alert("GAME OVER ! your score is  "+ result)
+    }
 }
 
-randomSquares()
+// let countDownTimerId = setInterval(countDown , 1000)
